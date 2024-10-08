06506275c420072988b0163aba031dae6c908cb6
Allan Mathenge <allanmathenge67@gmail.com>
2024-10-08 14:26:17 +0300

    updated the main activity and added the viewmodel

--- app/src/main/java/com/example/contentproviderbasics/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderbasics/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.contentproviderbasics
 
+import android.content.ContentUris
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,36 +17,65 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.contentproviderbasics.ui.theme.ContentProviderBasicsTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME
+        )
+
+        val millisYesterdy = Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_YEAR, -1)
+        }.timeInMillis
+
+        val selection = "${MediaStore.Images.Media.DATE_TAKEN} >= ?"
+
+        val selectionArgs = arrayOf(millisYesterdy.toString())
+
+        val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use { cursor->
+            val idColumn = cursor.getColumnIndex( MediaStore.Images.Media._ID)
+            val nameColumn = cursor.getColumnIndex( MediaStore.Images.Media.DISPLAY_NAME)
+
+            val images = mutableListOf<Image>()
+            while(cursor.moveToNext()){
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                images.add(Image(id,name,uri))
+
+            }
+        }
+
         setContent {
             ContentProviderBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class Image(
+        val id: Long,
+        val name: String,
+        val uri: Uri
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ContentProviderBasicsTheme {
-        Greeting("Android")
-    }
-}
+
