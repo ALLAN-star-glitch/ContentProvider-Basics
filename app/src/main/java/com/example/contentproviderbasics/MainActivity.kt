88efa38886be28fde4b84032058ec8178a9132d7
Allan Mathenge <allanmathenge67@gmail.com>
2024-10-08 15:25:23 +0300

    updated the main activity

--- app/src/main/java/com/example/contentproviderbasics/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderbasics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.contentproviderbasics
 
+import android.Manifest
 import android.content.ContentUris
 import android.net.Uri
 import android.os.Build
@@ -8,22 +9,38 @@ import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import coil3.compose.AsyncImage
 import com.example.contentproviderbasics.ui.theme.ContentProviderBasicsTheme
 import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ImageViewModel>()
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+            0
+        )
 
         val projection = arrayOf(
             MediaStore.Images.Media._ID,
@@ -31,7 +48,7 @@ class MainActivity : ComponentActivity() {
         )
 
         val millisYesterdy = Calendar.getInstance().apply {
-            add(Calendar.DAY_OF_YEAR, -1)
+            add(Calendar.DAY_OF_YEAR, -2)
         }.timeInMillis
 
         val selection = "${MediaStore.Images.Media.DATE_TAKEN} >= ?"
@@ -62,10 +79,33 @@ class MainActivity : ComponentActivity() {
                 images.add(Image(id,name,uri))
 
             }
+
+            viewModel.updateImages(images)
         }
 
         setContent {
             ContentProviderBasicsTheme {
+
+                LazyColumn (
+                    modifier = Modifier.fillMaxSize()
+                ){
+
+                    items(viewModel.images){image->
+                        Column (
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            AsyncImage(
+                                model = image.uri,
+                                contentDescription = null
+                                )
+                            Text(text = image.name )
+
+                        }
+
+                    }
+
+                }
 
             }
         }
